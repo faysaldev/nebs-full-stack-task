8c76cd14aba5a94690c90d168bfcde819295abe4
Faysal Mridha <fmridha166@gmail.com>
2025-12-10 16:25:29 +0600

    [feat]: added the shadcn libray into projects

---
+++ front-end/src/Page/Dashboard/CreateNoticePage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateNoticePage() {
+  return <div>CreateNoticePage</div>;
+}
+
+export default CreateNoticePage;
---
+++ front-end/src/components/Dashboard/PublishPopup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PublishPopup() {
+  return <div>PublishPopup</div>;
+}
+
+export default PublishPopup;
